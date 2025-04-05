12ef865e16683473d0501d21ce4906f3daee0bd9
sriramede9 <only4programming03@gmail.com>
2025-04-05 09:17:50 -0400

    updated style movie name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
 
     return <nav className={`navbar ${theme}-theme`}>
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/" className="nav-link">Movie App</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
